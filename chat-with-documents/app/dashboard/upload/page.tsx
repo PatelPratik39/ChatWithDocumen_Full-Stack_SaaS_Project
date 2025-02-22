d5dc0b8b9f21f7f7c8864a34923f0534a1e1a69a
PatelPratik39 <ptl.pratik717@gmail.com>
2025-02-21 23:07:22 -0600

    half app is ready and ready for deploy yo Varcel

--- chat-with-documents/app/dashboard/upload/page.tsx
+++ chat-with-documents/app/dashboard/upload/page.tsx
@@ -1,9 +1,11 @@
+import FileUploader from '@/components/FileUploader'
 import React from 'react'
 
 const UploadPage = () => {
   return (
       <div>
         {/* File Uploader */}
+        <FileUploader />
       </div>
   )
 }
