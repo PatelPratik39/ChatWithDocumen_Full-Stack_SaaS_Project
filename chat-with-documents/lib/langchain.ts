467fde9d648ae272866b24c12100f525c160824d
PatelPratik39 <ptl.pratik717@gmail.com>
2025-03-05 01:21:58 -0600

    imports for Pinecone

--- chat-with-documents/lib/langchain.ts
+++ chat-with-documents/lib/langchain.ts
@@ -1,4 +1,11 @@
-const model = new ChatOpenAI({
+
+import {ChatOpenAI} from '@langchain/openai';
+import {PDFLoader} from '@langchain/document_loaders/fs/pdf';
+import { RecursiveCharacterTextSplitterParams  } from "@langchain/textsplitters";
+import { OpenAIEmbeddings } from '@langchain/openai';
+import {createStuffDocumentsChain} from '@langchain/chains/combine_documents';
+
+const model = new ChatOpenAI({  
     openAIApiKey: process.env.OPENAI_API_KEY,
     modelName: 'gpt-4o',
 })
